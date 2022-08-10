d0089614aa8605662d74349c5257f74e9da71e8f
Ignacio <alzugaray.ign@gmail.com>
2022-08-10 14:49:21 +0100

    vscode

--- ubuntu2204_fresh_install.sh
+++ ubuntu2204_fresh_install.sh
@@ -9,9 +9,18 @@ echo_section_header "Ubuntu Basics"
 sudo apt install linux-headers-$(uname -r) # Kernel headers
 sudo apt install -y build-essential # GCC
 
+# Git
 echo_section_header "GIT & Public SSH key"
 bash git_with_ssh.sh
 
 # Conda
 echo_section_header "Conda"
 bash conda.sh
+
+# VSCode
+sudo snap install --classic code 
+code --install-extension ms-python.python 
+code --install-extension ms-toolsai.jupyter 
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cmake-tools
