52164b2e16c370328a03480271d0b68e5fc356b4
Ignacio <alzugaray.ign@gmail.com>
2024-07-03 12:51:16 +0100

    Update reconda

--- reconda.sh
+++ reconda.sh
@@ -2,10 +2,35 @@
 # Usage: source reconda.sh
 eval "$(conda shell.bash hook)"
 
-filename="environment.yaml"
-line=$(head -n 1 $filename)
+if [[ $# -eq 2 ]] && [[ $1 == "--install" ]]; then
+
+        alias_str="alias reconda=\"bash $PWD/reconda.sh\""
+	if [[ $2 == "bash" ]] || [[ 2 == "sh" ]]; then 
+		echo "Installing for bash"
+		echo $alias_str >> ~/.bashrc
+	elif [[ $2 == "zsh" ]]; then 
+		echo "Installing for zsh"
+		echo $alias_str >> ~/.zshrc
+	else
+		echo "Unknown terminal type"
+		exit 1
+	fi
+	echo "Installation done"
+	exit 0
+fi
+
+if test -f "environment.yaml"; then
+	env_filename="environment.yaml"
+elif test -f "environment.yml"; then
+	env_filename="environment.yml"
+else 
+	echo "Environment file not found"
+	exit 1
+fi
+
+
+line=$(head -n 1 $env_filename)
 echo $line
-env_filename="environment.yaml"
 env_name="$(grep -Po 'name: \K(.*)' $env_filename)"
 conda activate # Deactivate current environment into base
 conda env remove -y --name $env_name
